e365fbf85d864ff3a1a004aadb990cff09099d90
Ashot <sargsyan.ashot19@gmail.com>
2022-03-16 14:19:09 +0400

    added network class

---
+++ src/services/network/network.js
@@ -0,0 +1,88 @@
+import axios from 'axios';
+import qs from 'qs';
+import constants from "../settings/constant";
+
+class NetworkService {
+    constructor() {
+        this.source = null;
+    }
+
+    get = (url, options) => {
+        options = options || {};
+        return this.makeAPIRequest(url, options, 'GET');
+    };
+
+    post = (url, options) => {
+        options = options || {};
+        return this.makeAPIRequest(url, options, 'POST');
+    };
+
+    put = (url, options) => {
+        options = options || {};
+        return this.makeAPIRequest(url, options, 'PUT');
+    };
+
+    delete = (url, options) => {
+        options = options || {};
+        return this.makeAPIRequest(url, options, 'DELETE');
+    };
+
+    patch = (url, options) => {
+        options = options || {};
+        return this.makeAPIRequest(url, options, 'PATCH');
+    };
+
+    createUrl = (arg) => {
+        if (Array.isArray(arg)) {
+            return [constants.network.API_URL, ...arg].join('/');
+        }
+        return arg;
+    };
+
+    makeAPIRequest = (url, options, method) => {
+        return new Promise((resolve, reject) => {
+            if (!url) {
+                return reject('INVALID_REQUEST_PARAMS');
+            }
+
+            let _url = this.createUrl(url);
+
+            let fetch_options = {
+                url: _url,
+                method,
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                // data: options.data || {}, // this prop occurs CORS error
+                params: options.params || '',
+                paramsSerializer: (params) => qs.stringify(params),
+                timeout: options?.timeout ?? 10000,
+            };
+
+            if (options.headers) {
+                fetch_options.headers = options.headers;
+            }
+
+            axios(fetch_options)
+                .then(async (response) => {
+                    if (!response) {
+                        return reject('INVALID_RESPONSE_DATA');
+                    }
+
+                    let {data} = response;
+
+                    return resolve(data);
+                })
+                .catch((err) => {
+                    err = JSON.parse(JSON.stringify(err));
+                    if (err.message !== 'The user aborted a request.') {
+                        // let check_result = await this._validation_service.validate(data);
+                        return reject(err);
+                    }
+                });
+        })
+    }
+}
+
+export default new NetworkService();
